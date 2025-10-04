1585833ac64f3c6150b916cf53bf1469c8a1dee8
Jonathan Jimson <orbitron.hd.official@gmail.com>
2025-10-05 03:26:38 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer # type: ignore
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('publications.csv')
+df = pd.read_csv('publications.csv', encoding='latin-1')
 abstracts = df['abstract'].fillna('').tolist() 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
